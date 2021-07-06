43861261be2a364166b16301ce500e46fd08af97
Keian Rao <keian.rao@gmail.com>
2021-07-06 17:05:35 -0400

    Added.. "introductory" combobox loading.

--- BugSubmit/BugSubmit/src/app/app.component.ts
+++ BugSubmit/BugSubmit/src/app/app.component.ts
@@ -10,11 +10,18 @@ export class AppComponent {
   title = 'BugSubmit';
   statusMessage = "Loading data from the server. Pardon us for the wait..";
   ready = false;
+  projectInfo = [];
+  selectedProject = null;
+  selectedVersion = null;
+  selectedPlatform = null;
+  availableProjects = [];
+  availableVersions = [];
+  availablePlatforms = [];
   
   // Angular's generators are forcing a style of width-2 soft tabs.
   // Given this framework is quite opinionated, we should probably get
   // a text editor dedicated to its tastes..
-  
+    
   constructor(private projectInfoService: ProjectInfoService) {
   	this.fetchError = this.fetchError.bind(this);
 	this.integrateProjectInfo = this.integrateProjectInfo.bind(this);
@@ -37,30 +44,31 @@ export class AppComponent {
   fetchError() {
 	this.statusMessage =
 		"We tried to fetch project info from the database,"
-		+ "but were unsuccessful."
-		+ "Sorry; this bug reporter presently won't work.";
+		+ " but were unsuccessful."
+		+ " Sorry; this bug reporter presently won't work.";
 	// Does TypeScript enforce statement terminators?
 	// I expected compile errors from the above.
   }
   
   integrateProjectInfo(projectInfo) {
-  	// This is a bit tricky. We show versions only after a project
-	// is selected, and we show platforms only after a version is selected.
-	// And since ProjectInfoService didn't do it for us, we have to
-	// organise platforms hierarchically under versions too.
+	this.projectInfo = projectInfo;
+	this.availableProjects = this.getProjects(this.projectInfo);
+	// Rather than transform the data to a form suitable for us, I'm
+	// just going to live iterate everytime. The form we need (hierarchical
+	// dictionary keyed by name) is troublesome to transform this into.
 	
-	// For now let's pretend it worked. Bad.
 	return true;
   }
   
-  badProjectInfo() {
+  badProjectInfo(error) {
+  	console.error(error);
 	this.statusMessage =
 		"We fetched some project info, but they were bad."
-		+ "Sorry; this bug reporter presently won't work.";
+		+ " Sorry; this bug reporter presently won't work.";
   }
   
   enableForm() {
-  	this.statusMessage = "Successfully loaded.";
+  	this.statusMessage = "";
   	this.ready = true;
   }
   
@@ -68,7 +76,57 @@ export class AppComponent {
   	console.error(error);
 	this.statusMessage =
 		"Something quite unexpected occured."
-		+ "Sorry; this bug reporter presently won't work.";
+		+ " Sorry; this bug reporter presently won't work.";
+  }
+
+  selectProject = name => {
+  	this.selectedProject = name;
+	this.selectedVersion = null;
+	this.selectedPlatform = null;
+	this.availableVersions = this.getVersions(
+		this.projectInfo,
+		this.selectedProject
+	);
+	this.selectVersion(null);
+	this.selectPlatform(null);
+  }
+  
+  selectVersion = version => {
+  	this.selectedVersion = version;
+	this.selectedPlatform = null;
+	this.availablePlatforms = this.getPlatforms(
+		this.projectInfo, 
+		this.selectedProject,
+		this.selectedVersion
+	);
+	this.selectPlatform(null);
+  }
+  
+  selectPlatform = platform => {
+  	this.selectedPlatform = platform;
+  }
+  
+  alistFind(array, key, value) {
+  	return array.find(elem => elem[key] == value);
+  }
+  
+  getProjects(projectInfo) {
+  	return projectInfo.map(project => project.name);
+  }
+  
+  getVersions(projectInfo, project) {
+  	project = this.alistFind(projectInfo, "name", project);
+	if (!project) return [];
+  	return project.versions.map(version => version.version);
+  }
+  
+  getPlatforms(projectInfo, project, version) {
+  	project = this.alistFind(projectInfo, "name", project);
+	if (!project) return [];
+	return project.versions.reduce((platforms, elem) => {
+		if (elem.version == version) platforms.push(elem.platform);
+		return platforms;
+	}, []);
   }
   
 }
